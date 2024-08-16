8a370b5c07e617781c3756a06ef4c20dfb6c76e7
Gaondarech726 <anton.honcharenko2202@gmail.com>
2024-08-16 13:29:11 +0300

    tarasDR

---
+++ index.js
@@ -0,0 +1,36 @@
+let btn = document.querySelector("button");
+let taras = document.querySelector(".taras");
+let imgTaras = taras.querySelector(".img");
+let textTaras = taras.querySelector("p");
+let arrow = document.querySelector(".arrow");
+let arrowLeft = document.querySelector(".arrow__left");
+
+btn.addEventListener("click", () => {
+  const intervalId = setInterval(() => {
+    taras.style.animation = "an 2s linear forwards";
+
+    taras.addEventListener(
+      "animationend",
+      () => {
+        taras.style.animation = "";
+        clearInterval(intervalId);
+
+        taras.style.left = "0px";
+      },
+      { once: true }
+    );
+  }, 300);
+});
+arrow.addEventListener("click", () => {
+  if (
+    getComputedStyle(imgTaras, null).getPropertyValue("background-image") ==
+    `url("http://127.0.0.1:5500/Taras.jpg")`
+  ) {
+    imgTaras.style.backgroundImage = `url("./Taras2.jpg")`;
+  } else if (
+    getComputedStyle(imgTaras, null).getPropertyValue("background-image") ==
+    `url("http://127.0.0.1:5500/Taras2.jpg")`
+  ) {
+    imgTaras.style.backgroundImage = `url("./Taras.jpg")`;
+  }
+});
